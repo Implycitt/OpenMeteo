73f622e43bed4334f5cf00f3d8e5049fcbaf5a0b
Implycitt <qgbordelon@gmail.com>
2024-12-23 21:11:53 -0600

    setup to rewrite

--- src/scripts/geometry/Sphere.js
+++ src/scripts/geometry/Sphere.js
@@ -10,39 +10,24 @@ import {
     PointsMaterial,
     Points,
     Float32BufferAttribute,
-    SphereBufferGeometry,
-    MeshBasicMaterial,
 } from "three";
+
 import gsap from "gsap";
 import { InteractionManager } from "three.interactive";
-import ThreeGlobe from 'three-globe'
-import Globe from 'globe.gl'
 
 import earth_daymap from "../../assets/img/earth_daymap.jpg";
-import mars_map from "../../assets/img/mars.jpg";
 import earth_bump from "../../assets/img/earthbump.jpg";
-import mars_bump from "../../assets/img/marsBump.jpg";
 import earth_clouds from "../../assets/img/earthCloud.png";
 import star from "../../assets/img/stars.png";
 
 export const createSphere = (core) => {
     const { camera, renderer, scene } = core;
-
     const earthMaterial = new MeshPhongMaterial({
         map: new TextureLoader().load(earth_daymap),
         bumpMap: new TextureLoader().load(earth_bump),
         bumpScale: 2
     });
-
-    const marsMaterial = new MeshPhongMaterial({
-        map: new TextureLoader().load(mars_map),
-        bumpMap: new TextureLoader().load(mars_bump),
-        bumpScale: 2
-    });
-
     const sprite = new TextureLoader().load(star);
-    let markers;
-
     const starGeometry = new BufferGeometry();
     const starMaterial = new PointsMaterial({
         color: 0xffffff,
@@ -50,7 +35,6 @@ export const createSphere = (core) => {
         transparent: true,
         map: sprite
     });
-
     const starVertices = [];
     for (let i = 0; i < 10000; i++) {
         const x = (Math.random() - 0.5) * 2000;
@@ -58,55 +42,41 @@ export const createSphere = (core) => {
         const z = -Math.random() * 2000;
         starVertices.push(x, y, z);
     }
-
     starGeometry.setAttribute("position", new Float32BufferAttribute(starVertices, 3));
     const stars = new Points(starGeometry, starMaterial);
-    scene.add(stars);
-
     const cloudGeometry = new SphereGeometry(5.15, 100, 100);
-
     const cloudMaterial = new MeshPhongMaterial({
         map: new TextureLoader().load(earth_clouds),
         transparent: true
     });
-
     const point = new Points(starGeometry, starMaterial);
-    scene.add(point);
-
     const cloudMesh = new Mesh(cloudGeometry, cloudMaterial);
-    scene.add(cloudMesh);
-
     const ambientlight = new AmbientLight(0xffffff, 0.5);
-    scene.add(ambientlight);
-
-    const pointLight = new PointLight(0xffffff, 20);
-    pointLight.position.set(5, 3, 5);
-    scene.add(pointLight);
-
+    const pointLight = new PointLight(0xffffff, 20); pointLight.position.set(5, 3, 5);
     const geometry = new SphereGeometry(5, 100, 100);
-    const mars_geometry = new SphereGeometry(4, 100, 100);
     const earth = new Mesh(geometry, earthMaterial);
-    // const mars = new Mesh(mars_geometry, marsMaterial);
-    // mars.position.set(13, 0, -4);
-
-    scene.add(earth);
-    // scene.add(mars);
-
-    camera.position.z = 15;
-
     const group = new Group();
-
-    group.add(earth);
-    group.add(cloudMesh);
-    scene.add(group);
-
     const mouse = { x: null, y: null };
-
     const interactionManager = new InteractionManager(
         renderer,
         camera,
         renderer.domElement
     );
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    camera.position.z = 15;
+
+    scene.add(stars);
+    scene.add(point);
+    scene.add(cloudMesh);
+    scene.add(ambientlight);
+    scene.add(pointLight);
+    scene.add(point);
+    group.add(earth);
+    group.add(cloudMesh);
+    scene.add(group);
 
     interactionManager.add(earth);
 
@@ -114,10 +84,6 @@ export const createSphere = (core) => {
         mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
         mouse.y = (e.clientY / window.innerHeight) * 2 + 1;
     });
-
-    let isDown = false;
-    let startX;
-    let scrollLeft;
 
     group.addEventListener("mousedown", e => {
         isDown = true;
@@ -153,38 +119,6 @@ export const createSphere = (core) => {
         document.getElementById("container").removeAttribute("hidden");
         document.getElementById("searching").removeAttribute("hidden");
 
-        // document.getElementById("rem")?.addEventListener('keyup', (e) => {
-        //     if (e.key === "Enter") {
-        //         const search_city = "https://geocoding-api.open-meteo.com/v1/search";
-        //         const cityName = document.getElementById("rem").value;
-        //         fetch(`${search_city}?name=${cityName}`)
-        //         .then(response => {
-        //             return response.json();
-        //         })
-        //         .then(data => {
-        //             let lat = data.results[0].latitude;
-        //             let lon = data.results[0].longitude;
-                    
-        //             var latRad = lat * (Math.PI / 180);
-        //             var lonRad = -lon * (Math.PI / 180);
-        //             const N = 1;
-        //             var gData = [...Array(N).keys()].map(() => ({
-        //                 lat: Math.cos(latRad) * Math.cos(lonRad) * 5,
-        //                 lng: Math.sin(latRad) * 5,
-        //                 size: 0.5,
-        //                 color: 'red'
-        //             }));
-                    
-        //             // point.rotation.set(0.0, -lonRad, latRad - Math.PI * 0.5);
-                
-        //             var world = Globe()
-        //             console.log(gData)
-        //             world.pointsData(gData)
-        //             world.pointAltitude('size')
-        //             world.pointColor('red')
-        //         })
-        //     }
-        // });
 
         exit.addEventListener("click", () => {
             exit.setAttribute("hidden", "");
@@ -208,7 +142,6 @@ export const createSphere = (core) => {
 
     const animate = () => {
         earth.rotation.y += 0.001;
-        // mars.rotation.y += 0.001;
         cloudMesh.rotation.y += 0.001;
         gsap.to(group.rotation, { y: mouse.x * 0.5 });
 
--- src/scripts/modules/Canvas.js
+++ src/scripts/modules/Canvas.js
@@ -8,7 +8,6 @@ import {
 import { updateViewport } from "../utils/updateViewport";
 
 export const createCanvas = (core) => {
-    // Create the renderer.
     const canvas = document.querySelector("#earth-canvas");
     core.renderer = new WebGLRenderer({
         canvas,
@@ -17,14 +16,10 @@ export const createCanvas = (core) => {
         antialias: true
     });
 
-    // Renderer pixel ratio.
     core.renderer.setPixelRatio(window.devicePixelRatio);
-
     core.renderer.outputColorSpace = SRGBColorSpace;
     core.renderer.shadowMap.enabled = true;
-
     core.gl = canvas.getContext("webgl2");
-
     core.scene = new Scene();
     core.camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
